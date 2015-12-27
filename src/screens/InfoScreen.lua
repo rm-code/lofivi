a776c3c99940b711d6dc6a3359a7def4f779d86a
Robert Machmer <robert.machmer@gmail.com>
2015-12-27 13:54:15 +0100

    Merge InfoPanel into InfoScreen

--- src/screens/InfoScreen.lua
+++ src/screens/InfoScreen.lua
@@ -1,20 +1,34 @@
 local ScreenManager = require('lib.screenmanager.ScreenManager');
 local Screen = require('lib.screenmanager.Screen');
-local InfoPanel = require('src.ui.InfoPanel');
+
+-- ------------------------------------------------
+-- Constants
+-- ------------------------------------------------
+
+local INFO_MESSAGE = 'Drag a folder here to create a new graph!';
+
+local LABEL_FONT = love.graphics.newFont(25);
+local DEFAULT_FONT = love.graphics.newFont(12);
+
+-- ------------------------------------------------
+-- Module
+-- ------------------------------------------------
 
 local InfoScreen = {};
+
+-- ------------------------------------------------
+-- Constructor
+-- ------------------------------------------------
 
 function InfoScreen.new()
     local self = Screen.new();
 
-    local infoPanel = InfoPanel.new(love.graphics.getWidth() * 0.5, love.graphics.getHeight() * 0.5);
-
-    function self:update(dt)
-        infoPanel:update(dt);
-    end
+    love.window.setMode( 400, 400 );
 
     function self:draw()
-        infoPanel:draw();
+        love.graphics.setFont(LABEL_FONT);
+        love.graphics.printf(INFO_MESSAGE, 0, love.graphics.getHeight() * 0.5, 400, 'center');
+        love.graphics.setFont(DEFAULT_FONT);
     end
 
     function self:directorydropped(path)
